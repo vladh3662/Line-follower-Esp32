a1c02b9109e7076d339645ebf06abf8d767c03e8
vlad <vladh3662@gmail.com>
2025-05-21 18:43:26 +0300

    2

--- LIne.follower.ino
+++ LIne.follower.ino
@@ -12,7 +12,14 @@
 #define D4 25
 #define D5 14
 
-float lpos;
+float lpos = 3.0;
+float Kp = 35;
+float Ki = 0.4;
+float Kd = 15;
+
+float error = 0, lastError = 0, integral = 0;
+
+const int baseSpeed = 50;
 
 void setup() {
   pinMode(rmf, OUTPUT);
@@ -23,40 +30,17 @@ void setup() {
   pinMode(lms, OUTPUT);
   pinMode(rms, OUTPUT);
 
+  pinMode(D1, INPUT);
+  pinMode(D2, INPUT);
+  pinMode(D3, INPUT);
+  pinMode(D4, INPUT);
+  pinMode(D5, INPUT);
+
   Serial.begin(9600);
-}
-
-void forward() {
-  digitalWrite(rmf, HIGH);
-  digitalWrite(rmb, LOW);
-  digitalWrite(lmf, HIGH);
-  digitalWrite(lmb, LOW);
-
-  analogWrite(rms, 150);
-  analogWrite(lms, 150);
-}
-
-void turnRight() {
-  analogWrite(rms, 50);
-  analogWrite(lms, 150);
-  digitalWrite(rmf, HIGH);
-  digitalWrite(rmb, LOW);
-  digitalWrite(lmf, HIGH);
-  digitalWrite(lmb, LOW);
-}
-
-void turnLeft() {
-  analogWrite(rms, 150);
-  analogWrite(lms, 50);
-  digitalWrite(rmf, HIGH);
-  digitalWrite(rmb, LOW);
-  digitalWrite(lmf, HIGH);
-  digitalWrite(lmb, LOW);
 }
 
 void loop() {
   line_pos();
-  delay(10);
   motor();
   delay(5);
 }
@@ -67,15 +51,30 @@ void line_pos() {
   int s3 = digitalRead(D3);
   int s4 = digitalRead(D4);
   int s5 = digitalRead(D5);
-  if (s1 + s2 + s3 + s4 + s5 != 0)
-    lpos = (1 * s1 + 2 * s2 + 3 * s3 + 4 * s4 + 5 * s5) / (float)(s1 + s2 + s3 + s4 + s5);
+  int sum = s1 + s2 + s3 + s4 + s5;
+
+  if (sum != 0) {
+    lpos = (1 * s1 + 2 * s2 + 3 * s3 + 4 * s4 + 5 * s5) / (float)sum;
+  }
 }
 
 void motor() {
-  if (lpos <= 3.5 && lpos >= 2.5)
-    forward();
-  else if (lpos <= 5 && lpos >= 4)
-    turnRight();
-  else
-    turnLeft();
+  error = lpos - 3.0;
+  integral += error;
+  float derivative = error - lastError;
+
+  float correction = Kp * error + Ki * integral + Kd * derivative;
+
+  int leftSpeed = constrain(baseSpeed + correction, 0, 255);
+  int rightSpeed = constrain(baseSpeed - correction, 0, 255);
+
+  digitalWrite(rmf, HIGH);
+  digitalWrite(rmb, LOW);
+  digitalWrite(lmf, HIGH);
+  digitalWrite(lmb, LOW);
+
+  analogWrite(rms, rightSpeed);
+  analogWrite(lms, leftSpeed);
+
+  lastError = error;
 }
